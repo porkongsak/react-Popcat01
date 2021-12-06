776b32a3c1cca4343e44022e786f8de6cd470604
Kongsak <death01_por@hotmail.com>
2021-12-07 02:53:06 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState,useEffect } from 'react'
+import reactDom from 'react-dom'
+import audio from './pop.mp3'
+import img1 from './pop1.jpg'
+import img2 from './pop2.jpg'
+import './App.css'
+
+ function App() {
+
+  const [img, setImg]  = useState(img2)
+  const [number, setNumber] = useState(0)
+
+  //  useEffect(()=>{
+    
+  //  }, [img])
+  
+  const handleClick = (e) =>{
+    if (e.type === "mousedown") {
+      e.preventDefault();
+      setImg(img1);
+      new Audio(audio).play();
+      setNumber(number + 1);
+  } else{
+    e.preventDefault();
+    setImg(img2);
+  }
+
+  //  if(event.type === "mouseup") {
+  //    setImg(img2)
+  //   }   
+  }
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <header className="container">
+      <div className="container-fornt" >
+      <h1 style={{
+         color: "green",
+         textAlign: "center",
+         poSition: "absolute",
+         
+         }}  >POPCAT.....</h1> 
+         <h2 style={{
+         textAlign: "center",
+         poSition: "relative" 
+         }} >{number}</h2>
+         
+      <div className="imgchang" onMouseUp={handleClick} onMouseDown={handleClick}  >
+          {/* {img.data ? <img  src={img1} alt="" />:<img src={img2} alt="" />}    */}
+           <img className="img-cat" src={img}    />
+           
+          </div>
+      </div>
+    </header>
+    </>
+  )
 }
 
 export default App;
